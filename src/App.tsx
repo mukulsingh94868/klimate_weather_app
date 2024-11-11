532170ea990f1ff43e0bbb4d6a629cf265615c4c
mukul65 <mukul.singh@zentekinfosoft.com>
2024-11-11 16:07:10 +0530

    install dependencies for next js and typesciprt

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
 
   return (
     <>
       Hello World
+      <Button>Hello World</Button>
     </>
   )
 }
