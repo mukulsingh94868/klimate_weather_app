980d04902a1200bee203b160ae3b20e4a4d9ce09
mukulsingh94868 <mukulsingh94868@gmail.com>
2024-11-19 14:49:32 +0530

    weather details component

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -3,6 +3,7 @@ import HourlyTemperature from "@/components/hourly-temperature";
 import WeatherSkeleton from "@/components/loading-skeleton";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
+import WeatherDetails from "@/components/weather-details";
 import { useGeolocation } from "@/hooks/use-geolocation";
 import { useForecastQuery, useReverseGeocodeQuery, useWeatherQuery } from "@/hooks/use-weather";
 import { AlertTriangle, MapPin, RefreshCw } from "lucide-react";
@@ -109,8 +110,8 @@ const WeatherDashboard = () => {
                 </div>
 
                 <div className="grid gap-6 md:grid-cols-2 items-start">
-                    {/* <WeatherDetails data={weatherQuery.data} />
-                    <WeatherForecast data={forecastQuery.data} /> */}
+                    <WeatherDetails data={weatherQuery.data} />
+                    {/* <WeatherForecast data={forecastQuery.data} /> */}
                 </div>
             </div>
         </div>
