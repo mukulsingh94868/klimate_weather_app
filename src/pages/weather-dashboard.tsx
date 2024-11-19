5ba86ddfe6301b90ec37dae63d6d15fbd343a581
mukulsingh94868 <mukulsingh94868@gmail.com>
2024-11-19 15:13:36 +0530

    5 Day Forecast weather forecast component

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -4,6 +4,7 @@ import WeatherSkeleton from "@/components/loading-skeleton";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
 import WeatherDetails from "@/components/weather-details";
+import WeatherForecast from "@/components/weather-forecast";
 import { useGeolocation } from "@/hooks/use-geolocation";
 import { useForecastQuery, useReverseGeocodeQuery, useWeatherQuery } from "@/hooks/use-weather";
 import { AlertTriangle, MapPin, RefreshCw } from "lucide-react";
@@ -111,7 +112,7 @@ const WeatherDashboard = () => {
 
                 <div className="grid gap-6 md:grid-cols-2 items-start">
                     <WeatherDetails data={weatherQuery.data} />
-                    {/* <WeatherForecast data={forecastQuery.data} /> */}
+                    <WeatherForecast data={forecastQuery.data} />
                 </div>
             </div>
         </div>
